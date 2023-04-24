b246c9381f9fb9a1841711a9204b6789d6f26727
Scott Morgan <scott@adligo.com>
2023-04-23 23:45:35 -0500

    This is performing the slinks correctly now yea

--- buildEtc.cjs
+++ buildEtc.cjs
@@ -13,15 +13,22 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
-const { spawnSync } = require('child_process');
+const { execSync, spawnSync } = require('child_process');
 const IS_WIN = process.platform === "win32";
+console.log('IS_WIN? = ' + IS_WIN);
 var npm = 'npm'
 if (IS_WIN) {
   npm = 'npm.cmd'
 }
 
-function out(cmd, spawnSyncReturns) {
+function out(cmd, spawnSyncReturns, options) {
   console.log('ran ' + cmd );
+  if (options != undefined) {
+    //console.log('\twith options ' + JSON.stringify(options));
+    if (options.cwd != undefined) {
+      console.log('\tin ' + options.cwd);
+    } 
+  }
   console.log('\tand the spawnSyncReturns had;');
   if (spawnSyncReturns.error != undefined) {
     console.log('\tError: ' + spawnSyncReturns.error);
@@ -32,17 +39,15 @@ function out(cmd, spawnSyncReturns) {
   }
   if (spawnSyncReturns.stdout != undefined) {
     console.log('\tStdout: ' + spawnSyncReturns.stdout);
+    return spawnSyncReturns.stdout;
   }
+  console.log('\tStdout: ' + spawnSyncReturns);
+  return '' + spawnSyncReturns;
 }
 
-function run(cmd, args) {
+function run(cmd, options) {
   var cc = cmd;
-  if (args != undefined) {
-    for (var i=0; i < args.length; i++) {
-      cc = cc + ' ' + args[i];
-    }
-  }
-  out(cc, spawnSync(cmd, args));
+  return out(cc, execSync(cmd, options), options);
 }
 
 
@@ -58,7 +63,58 @@ const projects = ['cli.ts.adligo.org',
 'tests4j.ts.adligo.org',
 'xml.tests4j.ts.adligo.org'];
 
+function toUnixPath(path) {
+  var r = '';
+  var i=0;
+  if (path.charAt(1) == ':') {
+    r = '/' + path.charAt(0) + '/';
+    i = 3;
+  }
+  for (; i< path.length; i++) {
+    let c = path[i];
+    if (c == '\\') {
+      r = r.concat('/');
+    } else if (c == ' ') {
+      throw Error('Paths with spaces are NOT allowed in the following path ! ' + path);
+    } else {
+      r = r.concat(c);
+    }
+  }
+  return r;
+}
+
+function toWindowsPath(path) {
+  var r = '';
+  var i=0;
+  for (; i< path.length; i++) {
+    let c = path[i];
+    if (c == '/') {
+      r = r.concat('\\');
+    } else if (c == ' ') {
+      throw Error('Paths with spaces are NOT allowed in the following path ! ' + path);
+    } else {
+      r = r.concat(c);
+    }
+  }
+  return r;
+}
+
+function toOSPath(path) {
+  if (IS_WIN) {
+    return toWindowsPath(path);
+  } else {
+    return toUnixPath(path);
+  }
+}
+
+const dir = process.cwd();
+var ps = '/';
+if (IS_WIN) {
+  ps = '\\';
+}
+
 function gitClone(ssh) {
+  console.log('in gitClone ' + ssh);
   var base =  'https://github.com/';
   if (ssh) {
     base = 'git@github.com:'
@@ -68,12 +124,37 @@ function gitClone(ssh) {
   });
 }
 
-for (var i=0; i < process.argv; i++) {
-  let arg = argv[i];
-  if (arg == '--git-clone') {
-    gitClone(false);
+function slink() {
+  for(var i=0; i < projects.length; i++) {
+    let p = projects[i];
+    let pdir = dir + ps + p;
+    let osdir = toOSPath(pdir);
+    let udir = toUnixPath(pdir);
+    //let cmd = 'slink --debug --dir '+ udir;
+    let cmd = 'slink --version --debug';
+    //run('pwd',{ cwd: osdir});
+    //run(cmd,{ cwd: osdir});
+    //run('which slink',{ cwd: osdir});
+    run('slink --dir ' + udir,{ cwd: osdir});
   }
-  if (arg == '--git-clone-ssh') {
-    gitClone(true);
+}
+
+
+const args = process.argv;
+console.log('processing args ' + args.length);
+console.log('in ' + dir)
+//run('slink',{ cdw:  'C:\\Users\\scott\\org-src\\fr_group.ts.adligo.org\\tests4j.ts.adligo.org'});
+try {
+  for (var i=2; i < args.length; i++) {
+    let arg = args[i];
+    console.log('processing arg ' + arg);
+    switch (arg) {
+      case "--git-clone": gitClone(false); break;
+      case "--git-clone-ssh": gitClone(true); break;
+      case "--slink": slink(); break;
+      default: throw Error('unknown command '+ arg);
+    }
   }
+} catch (e) {
+  console.error(e.message);
 }
